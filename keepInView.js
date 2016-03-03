93649bd49680d4993d3ffce8f4e76f252431cfca
JR <jr@codebakery.ch>
2016-03-03 11:35:16 +0100

    Create keepInView.js

---
+++ keepInView.js
@@ -0,0 +1,25 @@
+.directive('keepInView', ['$window', function ($window) {
+    return {
+        restrict: 'AE',
+        scope: {
+            elm: '@', // elm to check offset for
+            togglePos: '@', // value after which the element gets a new class
+            toggleClass: '@' // class to toggle when after @elm and after @hideAfter
+        },
+        link: function (scope, element, attrs) {
+
+            var elmOffset = $(element).offset();
+            var posCheck = window.pageYOffset < elmOffset.top;
+            $(element).toggleClass(scope.hideClass, posCheck);
+            var doToggle = function () {
+                elmOffset = $(element).offset();
+                posCheck = window.pageYOffset < elmOffset.top;
+                $(element).toggleClass(scope.hideClass, posCheck);
+            };
+            doToggle();
+            angular.element($window).bind("scroll resize", function () {
+                doToggle();
+            });
+        }
+    };
+}])
